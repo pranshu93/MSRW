6d1a759a22fa54ac90d239ee49f0ee355d6d99cd
Dhrubojyoti Roy <royd@cse.osu.edu>
2018-10-17 02:04:05 -0400

    Splitting hyperparams file into 32 splits 5-fold in each split

---
+++ pbs_hpc/train_hyperparams_19.sh
@@ -0,0 +1,6 @@
+outname=`echo $0 | sed "s/.sh/.out/g"`
+python3 ../Architecture/dynamic_rnn.py -ggnl quantTanh -gunl quantTanh -ur 0.25 -wr 0.75 -w 32 -sp 0.5 -lr 0.005 -bs 128 -hs 16 -ot 0 -ml 768 -fn 1 -out $outname
+python3 ../Architecture/dynamic_rnn.py -ggnl quantTanh -gunl quantTanh -ur 0.25 -wr 0.75 -w 32 -sp 0.5 -lr 0.005 -bs 128 -hs 16 -ot 0 -ml 768 -fn 2 -out $outname
+python3 ../Architecture/dynamic_rnn.py -ggnl quantTanh -gunl quantTanh -ur 0.25 -wr 0.75 -w 32 -sp 0.5 -lr 0.005 -bs 128 -hs 16 -ot 0 -ml 768 -fn 3 -out $outname
+python3 ../Architecture/dynamic_rnn.py -ggnl quantTanh -gunl quantTanh -ur 0.25 -wr 0.75 -w 32 -sp 0.5 -lr 0.005 -bs 128 -hs 16 -ot 0 -ml 768 -fn 4 -out $outname
+python3 ../Architecture/dynamic_rnn.py -ggnl quantTanh -gunl quantTanh -ur 0.25 -wr 0.75 -w 32 -sp 0.5 -lr 0.005 -bs 128 -hs 16 -ot 0 -ml 768 -fn 5 -out $outname
