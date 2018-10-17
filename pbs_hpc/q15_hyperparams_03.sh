10cf40a45d7ed12aab2869c5eac0aee41aef55a8
Dhrubojyoti Roy <royd@cse.osu.edu>
2018-10-17 15:02:44 -0400

    Adding Adam optimizer to pbs scripts

--- pbs_hpc/q15_hyperparams_03.sh
+++ pbs_hpc/q15_hyperparams_03.sh
@@ -4,3 +4,8 @@ python3 ../Architecture/dynamic_rnn.py -ggnl quantSigm -gunl quantTanh -ur 0.25 
 python3 ../Architecture/dynamic_rnn.py -ggnl quantSigm -gunl quantTanh -ur 0.25 -wr 0.75 -w 32 -sp 0.5 -lr 0.005 -bs 128 -hs 16 -ot 0 -ml 768 -fn 3 -q15 True -out $outname
 python3 ../Architecture/dynamic_rnn.py -ggnl quantSigm -gunl quantTanh -ur 0.25 -wr 0.75 -w 32 -sp 0.5 -lr 0.005 -bs 128 -hs 16 -ot 0 -ml 768 -fn 4 -q15 True -out $outname
 python3 ../Architecture/dynamic_rnn.py -ggnl quantSigm -gunl quantTanh -ur 0.25 -wr 0.75 -w 32 -sp 0.5 -lr 0.005 -bs 128 -hs 16 -ot 0 -ml 768 -fn 5 -q15 True -out $outname
+python3 ../Architecture/dynamic_rnn.py -ggnl quantSigm -gunl quantTanh -ur 0.25 -wr 0.75 -w 32 -sp 0.5 -lr 0.005 -bs 128 -hs 16 -ot 1 -ml 768 -fn 1 -q15 True -out $outname
+python3 ../Architecture/dynamic_rnn.py -ggnl quantSigm -gunl quantTanh -ur 0.25 -wr 0.75 -w 32 -sp 0.5 -lr 0.005 -bs 128 -hs 16 -ot 1 -ml 768 -fn 2 -q15 True -out $outname
+python3 ../Architecture/dynamic_rnn.py -ggnl quantSigm -gunl quantTanh -ur 0.25 -wr 0.75 -w 32 -sp 0.5 -lr 0.005 -bs 128 -hs 16 -ot 1 -ml 768 -fn 3 -q15 True -out $outname
+python3 ../Architecture/dynamic_rnn.py -ggnl quantSigm -gunl quantTanh -ur 0.25 -wr 0.75 -w 32 -sp 0.5 -lr 0.005 -bs 128 -hs 16 -ot 1 -ml 768 -fn 4 -q15 True -out $outname
+python3 ../Architecture/dynamic_rnn.py -ggnl quantSigm -gunl quantTanh -ur 0.25 -wr 0.75 -w 32 -sp 0.5 -lr 0.005 -bs 128 -hs 16 -ot 1 -ml 768 -fn 5 -q15 True -out $outname
--- pbs_hpc/q15_hyperparams_23.sh
+++ pbs_hpc/q15_hyperparams_23.sh
@@ -4,3 +4,8 @@ python3 ../Architecture/dynamic_rnn.py -ggnl quantTanh -gunl quantTanh -ur 0.75 
 python3 ../Architecture/dynamic_rnn.py -ggnl quantTanh -gunl quantTanh -ur 0.75 -wr 0.75 -w 32 -sp 0.5 -lr 0.005 -bs 128 -hs 16 -ot 0 -ml 768 -fn 3 -q15 True -out $outname
 python3 ../Architecture/dynamic_rnn.py -ggnl quantTanh -gunl quantTanh -ur 0.75 -wr 0.75 -w 32 -sp 0.5 -lr 0.005 -bs 128 -hs 16 -ot 0 -ml 768 -fn 4 -q15 True -out $outname
 python3 ../Architecture/dynamic_rnn.py -ggnl quantTanh -gunl quantTanh -ur 0.75 -wr 0.75 -w 32 -sp 0.5 -lr 0.005 -bs 128 -hs 16 -ot 0 -ml 768 -fn 5 -q15 True -out $outname
+python3 ../Architecture/dynamic_rnn.py -ggnl quantTanh -gunl quantTanh -ur 0.75 -wr 0.75 -w 32 -sp 0.5 -lr 0.005 -bs 128 -hs 16 -ot 1 -ml 768 -fn 1 -q15 True -out $outname
+python3 ../Architecture/dynamic_rnn.py -ggnl quantTanh -gunl quantTanh -ur 0.75 -wr 0.75 -w 32 -sp 0.5 -lr 0.005 -bs 128 -hs 16 -ot 1 -ml 768 -fn 2 -q15 True -out $outname
+python3 ../Architecture/dynamic_rnn.py -ggnl quantTanh -gunl quantTanh -ur 0.75 -wr 0.75 -w 32 -sp 0.5 -lr 0.005 -bs 128 -hs 16 -ot 1 -ml 768 -fn 3 -q15 True -out $outname
+python3 ../Architecture/dynamic_rnn.py -ggnl quantTanh -gunl quantTanh -ur 0.75 -wr 0.75 -w 32 -sp 0.5 -lr 0.005 -bs 128 -hs 16 -ot 1 -ml 768 -fn 4 -q15 True -out $outname
+python3 ../Architecture/dynamic_rnn.py -ggnl quantTanh -gunl quantTanh -ur 0.75 -wr 0.75 -w 32 -sp 0.5 -lr 0.005 -bs 128 -hs 16 -ot 1 -ml 768 -fn 5 -q15 True -out $outname
