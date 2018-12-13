6b8281a4aab13842130925512ec0d2622ba2bdf1
Dhrubojyoti Roy <roy.174@buckeyemail.osu.edu>
2018-12-13 17:35:29 -0500

    Converting to boilerplates for easy future use

--- m_30_n_128_sigpad_pbs.py
+++ m_30_n_128_sigpad_pbs.py
@@ -1,40 +1,55 @@
 import sys
 import os
 
-# Enter pbs_hpc folder
-os.chdir('hpc_scripts')
-
+######################### ONLY MODIFY THESE VALUES #########################
 # Script prefix
 prefix='m_30_n_128_sigpad'
 
-# Folder where jobs are saved
-jobfolder = '../pbs_hpc/'
+# Number of splits of hyperparam file
+num_splits='32'
 
 # Base path of data
 base='/fs/project/PAS1090/radar/Austere/Bora_New_Detector/Bora_new_det_aust_th_22_bgr_19_M_30_N_128_window_res_lookahead_last_wind_padded'
 
+# Batch system
+bat_sys='pbs'
+
+# Human and nonhuman folders
+hum_fold='austere_384_human'
+nhum_fold='austere_304_cow'
+
+# Running time
+walltime='3:00:00'
+
+######################### KEEP THE REST INTACT #########################
+# Folder where jobs are saved
+jobfolder = '../'+ bat_sys +'_hpc/'
+
 #Init args
 init_argv=sys.argv
 
+# Enter hpc_scripts folder
+os.chdir('hpc_scripts')
+
 # Prepare data
 print('###### Scripts/processing_data #####')
-sys.argv=init_argv+['-type', prefix, '-base', base, '-hum','austere_384_human','-nhum','austere_304_cow']
+sys.argv=init_argv+['-type', prefix, '-base', base, '-hum', hum_fold, '-nhum', nhum_fold]
 import Scripts.processing_data
 
 # Generate gridsearch
 print('###### hpc_scripts/gridsearch #####')
-sys.argv=init_argv+['-type', prefix, '-bat', 'pbs', '-base', base]
+sys.argv=init_argv+['-type', prefix, '-bat', bat_sys, '-base', base]
 import hpc_scripts.gridsearch_0
 
 # Split hyperparam file
 print('###### hpc_scripts/split_hyp_wrapper #####')
-sys.argv=init_argv+[jobfolder+prefix+'.sh','32']
+sys.argv=init_argv+[jobfolder+prefix+'.sh',num_splits]
 import hpc_scripts.split_hyp_wrapper_1
 
 # Create batch job
 print('###### hpc_scripts/create_batch_wrapper #####')
-sys.argv=init_argv+[jobfolder+prefix+'_','3:00:00','pbs']
+sys.argv=init_argv+[jobfolder+prefix+'_',walltime,bat_sys]
 import hpc_scripts.create_batch_wrapper_2
 
 # Submit
-print("\nNow submit pbs_hpc/3_SUBMIT_"+prefix+"_jobs.sh on PBS server")
+print("\nNow submit " + bat_sys + "_hpc/3_SUBMIT_"+prefix+"_jobs.sh on server")
--- m_30_n_96_sigpad_pbs.py
+++ m_30_n_96_sigpad_pbs.py
@@ -1,40 +1,55 @@
 import sys
 import os
 
-print(os.getcwd())
-os.chdir('hpc_scripts')
-
+######################### ONLY MODIFY THESE VALUES #########################
 # Script prefix
 prefix='m_30_n_96_sigpad'
 
-# Folder where jobs are saved
-jobfolder = '../pbs_hpc/'
+# Number of splits of hyperparam file
+num_splits='32'
 
 # Base path of data
 base='/fs/project/PAS1090/radar/Austere/Bora_New_Detector/Bora_new_det_aus_M_30_N_96_win_res_last_w_padded_with_signal_lookahead'
 
+# Batch system
+bat_sys='pbs'
+
+# Human and nonhuman folders
+hum_fold='austere_386_human'
+nhum_fold='austere_310_cow'
+
+# Running time
+walltime='3:00:00'
+
+######################### KEEP THE REST INTACT #########################
+# Folder where jobs are saved
+jobfolder = '../'+ bat_sys +'_hpc/'
+
 #Init args
 init_argv=sys.argv
 
+# Enter hpc_scripts folder
+os.chdir('hpc_scripts')
+
 # Prepare data
 print('###### Scripts/processing_data #####')
-sys.argv=init_argv+['-type', prefix, '-base', base, '-hum','austere_386_human','-nhum','austere_310_cow']
+sys.argv=init_argv+['-type', prefix, '-base', base, '-hum', hum_fold, '-nhum', nhum_fold]
 import Scripts.processing_data
 
 # Generate gridsearch
 print('###### hpc_scripts/gridsearch #####')
-sys.argv=init_argv+['-type', prefix, '-bat', 'pbs', '-base', base]
+sys.argv=init_argv+['-type', prefix, '-bat', bat_sys, '-base', base]
 import hpc_scripts.gridsearch_0
 
 # Split hyperparam file
 print('###### hpc_scripts/split_hyp_wrapper #####')
-sys.argv=init_argv+[jobfolder+prefix+'.sh','32']
+sys.argv=init_argv+[jobfolder+prefix+'.sh',num_splits]
 import hpc_scripts.split_hyp_wrapper_1
 
 # Create batch job
 print('###### hpc_scripts/create_batch_wrapper #####')
-sys.argv=init_argv+[jobfolder+prefix+'_','3:00:00','pbs']
+sys.argv=init_argv+[jobfolder+prefix+'_',walltime,bat_sys]
 import hpc_scripts.create_batch_wrapper_2
 
 # Submit
-print("\nNow submit pbs_hpc/3_SUBMIT_"+prefix+"_jobs.sh on PBS server")
+print("\nNow submit " + bat_sys + "_hpc/3_SUBMIT_"+prefix+"_jobs.sh on server")
