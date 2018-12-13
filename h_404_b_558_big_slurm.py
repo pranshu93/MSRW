df98f7173c229c2c29ca6b6615a8049462275e3e
Dhrubojyoti Roy <royd@cse.osu.edu>
2018-12-13 18:50:45 -0500

    Adding boilerplate script for big gridsearch Activity dataset

---
+++ h_404_b_558_big_slurm.py
@@ -0,0 +1,58 @@
+import sys
+import os
+
+######################### ONLY MODIFY THESE VALUES #########################
+# Script prefix
+prefix='h_404_b_558'
+
+# Number of splits of hyperparam file
+num_splits='50'
+
+# Base path of data
+base='/scratch/dr2915/Austere/Activity'
+
+# Batch system
+bat_sys='slurm'
+
+# Human and nonhuman folders
+hum_fold='austere_404_human'
+nhum_fold='Bike_558'
+
+# Running time
+walltime='2-0'
+
+# Search big search space
+big_search='True'
+
+######################### KEEP THE REST INTACT #########################
+# Folder where jobs are saved
+jobfolder = '../'+ bat_sys +'_hpc/'
+
+#Init args
+init_argv=sys.argv
+
+# Enter hpc_scripts folder
+os.chdir('hpc_scripts')
+
+# Prepare data
+print('###### Scripts/processing_data #####')
+sys.argv=init_argv+['-type', prefix, '-base', base, '-hum', hum_fold, '-nhum', nhum_fold]
+import Scripts.processing_data
+
+# Generate gridsearch
+print('###### hpc_scripts/gridsearch #####')
+sys.argv=init_argv+['-type', prefix, '-bat', bat_sys, '-base', base, '-big', big_search]
+import hpc_scripts.gridsearch_0
+
+# Split hyperparam file
+print('###### hpc_scripts/split_hyp_wrapper #####')
+sys.argv=init_argv+[jobfolder+prefix+'_big.sh',num_splits]
+import hpc_scripts.split_hyp_wrapper_1
+
+# Create batch job
+print('###### hpc_scripts/create_batch_wrapper #####')
+sys.argv=init_argv+[jobfolder+prefix+'_big_',walltime,bat_sys]
+import hpc_scripts.create_batch_wrapper_2
+
+# Submit
+print("\nNow submit " + bat_sys + "_hpc/3_SUBMIT_"+prefix+"_big_jobs.sh on server")
