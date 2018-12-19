63adc228130ecc7ca4b52cc1cab553c37eeb6160
Dhrubojyoti Roy <roy.174@buckeyemail.osu.edu>
2018-12-19 14:50:35 -0500

    Adding slurm script for radial only bikes Austere

---
+++ h_404_b_485_radial_slurm.py
@@ -0,0 +1,55 @@
+import sys
+import os
+
+######################### ONLY MODIFY THESE VALUES #########################
+# Script prefix
+prefix='h_404_b_485_radial'
+
+# Number of splits of hyperparam file
+num_splits='32'
+
+# Base path of data
+base='/scratch/dr2915/Austere/Activity/'
+
+# Batch system
+bat_sys='pbs'
+
+# Human and nonhuman folders
+hum_fold='austere_404_human'
+nhum_fold='Bike_485_radial'
+
+# Running time
+walltime='3:00:00'
+
+######################### KEEP THE REST INTACT #########################
+# Folder where jobs are saved
+jobfolder = '../'+ bat_sys +'_hpc/'
+
+#Init args
+init_argv=sys.argv
+
+# Enter hpc_scripts folder
+os.chdir('hpc_scripts')
+
+# Prepare data
+print('###### Scripts/processing_data #####')
+sys.argv=init_argv+['-type', prefix, '-base', base, '-hum', hum_fold, '-nhum', nhum_fold]
+import Scripts.processing_data
+
+# Generate gridsearch
+print('###### hpc_scripts/gridsearch #####')
+sys.argv=init_argv+['-type', prefix, '-bat', bat_sys, '-base', base]
+import hpc_scripts.gridsearch_0
+
+# Split hyperparam file
+print('###### hpc_scripts/split_hyp_wrapper #####')
+sys.argv=init_argv+[jobfolder+prefix+'.sh',num_splits]
+import hpc_scripts.split_hyp_wrapper_1
+
+# Create batch job
+print('###### hpc_scripts/create_batch_wrapper #####')
+sys.argv=init_argv+[jobfolder+prefix+'_',walltime,bat_sys]
+import hpc_scripts.create_batch_wrapper_2
+
+# Submit
+print("\nNow submit " + bat_sys + "_hpc/3_SUBMIT_"+prefix+"_jobs.sh on server")
