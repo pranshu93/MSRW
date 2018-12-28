f5eb901aa5dc5c3759ef478cd7674fbbdf53f792
Dhrubojyoti Roy <roy.174@buckeyemail.osu.edu>
2018-12-28 16:46:57 -0500

    Making getFreqData a subfunction of genFreqPoints

--- CNN/genFreqPoints.m
+++ CNN/genFreqPoints.m
@@ -1,12 +1,57 @@
+function genFreqPoints(prefix, basepath, folder_neg, folder_pos, outpath)
 %num = 1000;
-window = 250;
+
+    function train_row = getFreqData(fileName, path_dir)
+        cd(path_dir);
+        [I,Q,N]=Data2IQ(ReadBin([fileName,'.data']));
+        
+        %dcI = 2044;   % enable when do test on dummy data
+        %dcQ = 2048;
+        dcI = median(I); %median or mean
+        dcQ = median(Q);
+        Data = (I-dcI) + 1i*(Q-dcQ);
+        
+        %Rate=256;
+        Rate=window;
+        FftWindow = Rate;
+        Nfft = Rate;
+        FftStep = round(1/4*FftWindow);
+        
+        %DeltaF = Rate/Nfft;
+        %RelFreq = Wrap([0 : Nfft-1], -Nfft/2, Nfft/2);
+        %Freq = DeltaF * RelFreq;
+        
+        %s = spectrogram(x,window,noverlap,nfft)
+        S = spectrogram(Data, FftWindow, FftWindow - FftStep, Nfft);
+        A = abs(S);
+        phi = angle(S);
+        A = A';
+        phi = phi';
+        train_row = horzcat(A,phi);
+        
+        %TimeFreq = spectrogram_nohamming(Data, FftWindow, FftWindow - FftStep, Nfft, Rate);
+        %numWindows = size(TimeFreq,2);
+        
+        %Out = zeros(numWindows,FftWindow);
+        %thr_sqr_matlab_log = 14.44;
+        %14.14->10, 14.14 is chosen in matlab, 10 in c#
+        %thr_sqr_matlab = 10^(thr_sqr_matlab_log/10)*25238; % because S^2/P = 25238
+        %x = TimeFreq';
+        
+        %for j = 1:FftWindow
+        %Out(:,j) =  abs(x(:,j)).^2 > thr_sqr_matlab;
+        %Out(:,j) =  abs(x(:,j)) > thr_sqr_matlab^0.5;
+        %end
+    end
+
+window = 256;
 remove = 0;
 remove_begin = 2;
 remove_last = remove_begin;
-total_remove = remove_begin + remove_last; 
+total_remove = remove_begin + remove_last;
 
-data_neg = 'C:\Users\Sangeeta\Desktop\MATLAB_Scripts\Data_Repository\Bike_Human\bikes_first_window_start'; % num2str(num)];
-data_pos = 'C:\Users\Sangeeta\Desktop\MATLAB_Scripts\Data_Repository\Bike_Human\humans_first_window_start'; % num2str(num)];
+data_neg = fullfile(basepath,folder_neg); %'C:\Users\Sangeeta\Desktop\MATLAB_Scripts\Data_Repository\Bike_Human\bikes_first_window_start'; % num2str(num)];
+data_pos = fullfile(basepath,folder_pos); %'C:\Users\Sangeeta\Desktop\MATLAB_Scripts\Data_Repository\Bike_Human\humans_first_window_start'; % num2str(num)];
 train_data = [];
 
 cd(data_neg);
@@ -15,13 +60,13 @@ negFiles={};  % first 2 file is '.' and '..'
 n = 1;
 
 for j=1:length(negFullNames)
-     s=negFullNames(j).name;
-     k=strfind(s,'.data');
-     if ~isempty(k) && k>=2 && k+4==length(s)
-         negFiles{n}=s(1:k-1);
-         n=n+1;
-     end
- end
+    s=negFullNames(j).name;
+    k=strfind(s,'.data');
+    if ~isempty(k) && k>=2 && k+4==length(s)
+        negFiles{n}=s(1:k-1);
+        n=n+1;
+    end
+end
 
 cd(data_pos);
 posFullNames=dir;
@@ -31,16 +76,16 @@ p = 1;
 for j=1:length(posFullNames)
     s=posFullNames(j).name;
     k=strfind(s,'.data');
-        if ~isempty(k) && k>=2 && k+4==length(s)
-            posFiles{p}=s(1:k-1);
-            p=p+1;
-        end
+    if ~isempty(k) && k>=2 && k+4==length(s)
+        posFiles{p}=s(1:k-1);
+        p=p+1;
+    end
 end
- 
+
 countpos = 0;
 countneg = 0;
 
-cd('C:\Users\Sangeeta\Desktop\MATLAB_Scripts\Scripts')
+%cd('C:\Users\Sangeeta\Desktop\MATLAB_Scripts\Scripts')
 for i=1:p-1
     d = getFreqData(posFiles{i}, data_pos);
     cp = size(d);
@@ -55,7 +100,7 @@ for i=1:p-1
     end
 end
 
-cd('C:\Users\Sangeeta\Desktop\MATLAB_Scripts\Scripts')
+%cd('C:\Users\Sangeeta\Desktop\MATLAB_Scripts\Scripts')
 for i=1:n-1
     d = getFreqData(negFiles{i}, data_neg);
     cn = size(d);
@@ -84,5 +129,8 @@ train(:,1:2:(window*2)-1) = cmag;
 train(:,2:2:window*2) = cphi;
 data = horzcat(train,train_labels);
 
-cd('C:\Users\Sangeeta\Desktop\MATLAB_Scripts\Data_Repository')
-csvwrite('bike_human_full.csv',data)
+%cd('C:\Users\Sangeeta\Desktop\MATLAB_Scripts\Data_Repository')
+cd(outpath);
+%csvwrite('bike_human_full.csv',data)
+csvwrite([prefix,'_spectrogram.csv'],data)
+end
