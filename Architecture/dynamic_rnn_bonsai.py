9f1d4275d3ceb98d6b4afbf6950df5f644fcdb69
Dhrubojyoti Roy <royd@cse.osu.edu>
2019-01-25 02:55:03 -0500

    Disabling IHT and LR reduce, adding noZ script

--- Architecture/dynamic_rnn_bonsai.py
+++ Architecture/dynamic_rnn_bonsai.py
@@ -10,8 +10,11 @@ from tensorflow.contrib import rnn
 from rnn import FastRNNCell,FastGRNNCell
 import utils as utils
 import sys
+import time
 
-#os.environ["CUDA_VISIBLE_DEVICES"]=""
+# Make GPU available
+os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
+os.environ["CUDA_VISIBLE_DEVICES"]="0"
 
 # Making sure MSRW is part of python path
 sys.path.insert(0, '../')
@@ -20,6 +23,217 @@ sys.path.insert(0, '../')
 
 np.random.seed(42)
 tf.set_random_seed(42)
+
+class FastRNNBonsai_NoZ:
+    def __init__(self, C, F, D, S, lW, lT, lV,
+                 sW, sT, sV, W=None, T=None, V=None):
+
+        self.dataDimension = F
+        self.projectionDimension = F # since no Z
+        if (C > 2):
+            self.numClasses = C
+        elif (C == 2):
+            self.numClasses = 1
+
+        self.treeDepth = D
+        self.sigma = S  # tf.Variable(S, name='sigma', dtype=tf.float32)
+        self.lW = lW
+        self.lV = lV
+        self.lT = lT
+        self.sW = sW
+        self.sV = sV
+        self.sT = sT
+
+        self.internalNodes = 2 ** self.treeDepth - 1
+        self.totalNodes = 2 * self.internalNodes + 1
+
+        self.W = self.initW(W)
+        self.V = self.initV(V)
+        self.T = self.initT(T)
+
+        self.W_th = tf.placeholder(tf.float32, name='W_th')
+        self.V_th = tf.placeholder(tf.float32, name='V_th')
+        self.T_th = tf.placeholder(tf.float32, name='T_th')
+
+        self.W_st = tf.placeholder(tf.float32, name='W_st')
+        self.V_st = tf.placeholder(tf.float32, name='V_st')
+        self.T_st = tf.placeholder(tf.float32, name='T_st')
+
+        #if x is None:
+        #    self.x = tf.placeholder("float", [None, self.dataDimension])
+        #else:
+        self.x = tf.placeholder("float", [None, seq_max_len, window])
+        self.y = tf.placeholder("float", [None, self.numClasses])
+        self.batch_th = tf.placeholder(tf.int64, name='batch_th')
+
+        self.sigmaI = 1.0
+
+        self.hardThrsd()
+        self.sparseTraining()
+        self.lossGraph()
+        self.trainGraph()
+        self.accuracyGraph()
+
+    def initW(self, W):
+        if W is None:
+            W = tf.random_normal([self.numClasses * self.totalNodes, self.projectionDimension])
+        W = tf.Variable(W, name='W', dtype=tf.float32)
+        return W
+
+    def initV(self, V):
+        if V is None:
+            V = tf.random_normal([self.numClasses * self.totalNodes, self.projectionDimension])
+        V = tf.Variable(V, name='V', dtype=tf.float32)
+        return V
+
+    def initT(self, T):
+        if T is None:
+            T = tf.random_normal([self.internalNodes, self.projectionDimension])
+        T = tf.Variable(T, name='T', dtype=tf.float32)
+        return T
+
+    # Commented by Dhrubo
+    '''
+    def getModelSize(self):
+        nnzZ = np.ceil(int(Z.shape[0]*Z.shape[1])*sZ)
+        nnzW = np.ceil(int(W.shape[0]*W.shape[1])*sW)
+        nnzV = np.ceil(int(V.shape[0]*V.shape[1])*sV)
+        nnzT = np.ceil(int(T.shape[0]*T.shape[1])*sT)
+        return (nnzZ+nnzT+nnzV+nnzW)*8
+    '''
+
+    def getModelSize(self):
+        nnzW = np.ceil(int(self.W.shape[0] * self.W.shape[1]) * self.sW)
+        nnzV = np.ceil(int(self.V.shape[0] * self.V.shape[1]) * self.sV)
+        nnzT = np.ceil(int(self.T.shape[0] * self.T.shape[1]) * self.sT)
+        return int((nnzT + nnzV + nnzW) * 8)
+
+
+    def bonsaiGraph(self, X):
+        # Fast(G)RNN
+        X = tf.unstack(X, seq_max_len, 1)
+        # rnn_cell = tf.contrib.rnn.BasicLSTMCell(hidden_dim)
+        if args.wr is None or args.ur is None:
+            wRank = None
+            uRank = None
+        else:
+            wRank = int(args.wr * min(window, hidden_dim))
+            uRank = int(args.ur * hidden_dim)
+
+        if (args.ct):
+            rnn_cell = FastGRNNCell(hidden_dim, gate_non_linearity=args.ggnl, update_non_linearity=args.gunl,
+                                    wRank=wRank, uRank=uRank)
+        else:
+            rnn_cell = FastRNNCell(hidden_dim, update_non_linearity=args.unl,
+                                   wRank=wRank, uRank=uRank)
+        # rnn_cell = tf.nn.rnn_cell.DropoutWrapper(rnn_cell,output_keep_prob=0.9)
+
+        outputs, states = tf.contrib.rnn.static_rnn(rnn_cell, X, dtype=tf.float32, sequence_length=seqlen)
+        outputs = tf.stack(outputs)
+        outputs = tf.transpose(outputs, [1, 0, 2])
+        #batch_size = tf.shape(outputs)[0]
+        #index = tf.range(0, batch_size) * seq_max_len + (seqlen - 1)
+        index = tf.range(0, tf.shape(outputs)[0]) * seq_max_len + (seqlen - 1)
+        X = tf.gather(tf.reshape(outputs, [-1, hidden_dim]), index)
+
+        # Bonsai
+        X_ = tf.reshape(X, [self.dataDimension, -1])
+        #X_ = tf.divide(tf.matmul(self.Z, X, transpose_b=True), self.projectionDimension)
+        # X_ = tf.nn.l2_normalize(tf.matmul(self.Z, X, transpose_b=True), 0)
+        W_ = self.W[0:(self.numClasses)]
+        V_ = self.V[0:(self.numClasses)]
+        self.nodeProb = []
+        self.nodeProb.append(1)
+        score_ = self.nodeProb[0] * tf.multiply(tf.matmul(W_, X_), tf.tanh(self.sigma * tf.matmul(V_, X_)))
+        for i in range(1, self.totalNodes):
+            W_ = self.W[i * self.numClasses:((i + 1) * self.numClasses)]
+            V_ = self.V[i * self.numClasses:((i + 1) * self.numClasses)]
+            prob = (1 + ((-1) ** (i + 1)) * tf.tanh(tf.multiply(self.sigmaI,
+                                                                tf.matmul(tf.reshape(self.T[int(np.ceil(i / 2) - 1)],
+                                                                                     [-1, self.projectionDimension]),
+                                                                          X_))))
+            prob = tf.divide(prob, 2)
+            prob = self.nodeProb[int(np.ceil(i / 2) - 1)] * prob
+            self.nodeProb.append(prob)
+            score_ = score_ + self.nodeProb[i] * tf.multiply(tf.matmul(W_, X_), tf.tanh(self.sigma * tf.matmul(V_, X_)))
+
+        return score_, X_, self.T, self.W, self.V
+
+    def hardThrsd(self):
+        self.W_op1 = self.W.assign(self.W_th)
+        self.V_op1 = self.V.assign(self.V_th)
+        self.T_op1 = self.T.assign(self.T_th)
+        self.hard_thrsd_grp = tf.group(self.W_op1, self.V_op1, self.T_op1)
+
+    def sparseTraining(self):
+        self.W_op2 = self.W.assign(self.W_st)
+        self.V_op2 = self.V.assign(self.V_st)
+        self.T_op2 = self.T.assign(self.T_st)
+        self.sparse_retrain_grp = tf.group(self.W_op2, self.V_op2, self.T_op2)
+
+    def lossGraph(self):
+        self.score, self.X_eval, self.T_eval, self.W_eval, self.V_eval = self.bonsaiGraph(self.x)
+
+        if (self.numClasses > 2):
+            self.margin_loss = utils.multiClassHingeLoss(tf.transpose(self.score), tf.argmax(self.y, 1), self.batch_th)
+            self.reg_loss = 0.5 * (self.lW * tf.square(tf.norm(self.W)) +
+                                   self.lV * tf.square(tf.norm(self.V)) + self.lT * tf.square(tf.norm(self.T)))
+            self.loss = self.margin_loss + self.reg_loss
+        else:
+            self.margin_loss = tf.reduce_mean(tf.nn.relu(1.0 - (2 * self.y - 1) * tf.transpose(self.score)))
+            self.reg_loss = 0.5 * (self.lW * tf.square(tf.norm(self.W)) +
+                                   self.lV * tf.square(tf.norm(self.V)) + self.lT * tf.square(tf.norm(self.T)))
+            self.loss = self.margin_loss + self.reg_loss
+
+    def trainGraph(self):
+        #self.train_stepW = (tf.train.AdamOptimizer(self.learning_rate).minimize(self.loss, var_list=[self.W]))
+        #self.train_stepV = (tf.train.AdamOptimizer(self.learning_rate).minimize(self.loss, var_list=[self.V]))
+        #self.train_stepT = (tf.train.AdamOptimizer(self.learning_rate).minimize(self.loss, var_list=[self.T]))
+        #self.train_stepZ = (tf.train.AdamOptimizer(self.learning_rate).minimize(self.loss, var_list=[self.Z]))
+        #self.train_step = optimizer.minimize(self.loss)
+        # Apply gradient clipping
+        gvs = optimizer.compute_gradients(self.loss)
+        capped_gvs = [(tf.clip_by_value(grad, -1., 1.), var) for grad, var in gvs]
+        self.train_step = optimizer.apply_gradients(capped_gvs)
+
+    def accuracyGraph(self):
+        if (self.numClasses > 2):
+            correct_prediction = tf.equal(tf.argmax(tf.transpose(self.score), 1), tf.argmax(self.y, 1))
+            self.accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+        else:
+            y_ = self.y * 2 - 1
+            correct_prediction = tf.multiply(tf.transpose(self.score), y_)
+            correct_prediction = tf.nn.relu(correct_prediction)
+            correct_prediction = tf.ceil(tf.tanh(correct_prediction))
+            self.accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+    def analyse(self):
+        _feed_dict = {self.x: train_feats, self.y: train_labels, seqlen: train_seqlen}
+        x_cap_eval = self.X_eval.eval(feed_dict=_feed_dict)
+        tt1 = self.T_eval.eval()
+        prob = []
+        for i in range(0, self.internalNodes):
+            prob.append(np.dot(tt1[i], x_cap_eval))
+        prob = np.array(prob)
+        nodes = np.zeros(self.internalNodes + 1)
+        for i in range(x_cap_eval.shape[1]):
+            j = 0
+            while j < self.internalNodes:
+                if (prob[j][i] > 0):
+                    if (2 * j + 1 < self.internalNodes):
+                        j = 2 * j + 1
+                    else:
+                        j = 2 * j + 1
+                        nodes[j - self.internalNodes] = nodes[j - self.internalNodes] + 1
+                else:
+                    if (2 * j + 2 < self.internalNodes):
+                        j = 2 * j + 2
+                    else:
+                        j = 2 * j + 2
+                        nodes[j - self.internalNodes] = nodes[j - self.internalNodes] + 1
+        for i in range(0, self.internalNodes + 1):
+            print(i, nodes[i])
+
 
 class FastRNNBonsai:
     def __init__(self, C, F, P, D, S, lW, lT, lV, lZ,
@@ -255,7 +469,7 @@ def getArgs():
     parser.add_argument('-wr', type=float, default=None, help='Rank of W matrix')
     parser.add_argument('-w', type=int, default=32, help='Window Length')
     parser.add_argument('-sp', type=float, default=0.5, help='Stride as % of Window Length(0.25/0.5/0.75/1)')
-    parser.add_argument('-lr', type=float, default=0.01, help='Learning Rate of Optimisation')
+    parser.add_argument('-lr', type=float, default=0.001, help='Learning Rate of Optimisation')
     parser.add_argument('-bs', type=int, default=128, help='Batch Size of Optimisation')
     parser.add_argument('-hs', type=int, default=16, help='Hidden Layer Size')
     parser.add_argument('-ot', type=int, default=1, help='Adam(False)/Momentum(True)')
@@ -263,11 +477,12 @@ def getArgs():
     parser.add_argument('-nc', type=int, default=2, help='Number of classes')
     parser.add_argument('-fn', type=int, default=3, help='Fold Number to classify for cross validation[1/2/3/4/5]')
     parser.add_argument('-q15', type=ast.literal_eval, default=False, help='Represent input as Q15?')
-    parser.add_argument('-out', type=str, default=sys.stdout, help='Output filename')
+    parser.add_argument('-out', type=str, default='out.txt', help='Output filename')
     parser.add_argument('-type', type=str, default='tar', help='Classification type: \'tar\' for target,' \
                                                                ' \'act\' for activity)')
     parser.add_argument('-sig', type=float, default=1.0, help='Sigma parameter in Bonsai')
     parser.add_argument('-dep', type=int, default=2, help='Depth of Bonsai tree')
+    parser.add_argument('-noZ', default=False, action='store_true', help='If enabled, projection matrix Z is excluded')
     parser.add_argument('-P', type=int, default=2, help='Bonsai projection dimension')
     parser.add_argument('-rZ', type=float, default=1.0, help='Bonsai regularization Z')
     parser.add_argument('-rT', type=float, default=1.0, help='Bonsai regularization Theta')
@@ -314,6 +529,9 @@ def process(data,labels):
 
 
 #### Main function ####
+# Start profiling
+start_time = time.time()
+
 args = getArgs()
 window = args.w
 stride = int(window * args.sp);
@@ -428,7 +646,11 @@ seqlen = tf.placeholder(tf.int32, [None])
 
 #features = dynamicRNNFeaturizer(X)
 # Connect Bonsai graph
-fastrnnbonsaiObj = FastRNNBonsai(num_classes, hidden_dim, projectionDimension, depth, sigma,
+if args.noZ:
+    fastrnnbonsaiObj = FastRNNBonsai_NoZ(num_classes, hidden_dim, depth, sigma,
+                                     regW, regT, regV, sparW, sparT, sparV)
+else:
+    fastrnnbonsaiObj = FastRNNBonsai(num_classes, hidden_dim, projectionDimension, depth, sigma,
                                  regW, regT, regV, regZ, sparW, sparT, sparV, sparZ)
 
 sess = tf.InteractiveSession()
@@ -449,11 +671,11 @@ else:
     trimlevel = 5
 iht_done = 0
 
+cur_learningRate = args.lr
+
 for i in range(num_epochs):
-    if i==0:
-        cur_learningRate = args.lr
-    elif i % (num_epochs/5) == 0:
-        cur_learningRate = cur_learningRate / 10
+    #if i % (num_epochs/10) == 0 and i > 0:
+    #    cur_learningRate = cur_learningRate / 10
 
     accu = 0.0
     for j in range(num_iters):
@@ -506,16 +728,18 @@ for i in range(num_epochs):
         temp = fastrnnbonsaiObj.accuracy.eval(feed_dict=_feed_dict)
         accu = temp + accu
 
-        if counter >= total_batches / 3 and counter < 2 * total_batches / 3:
+        '''if counter >= total_batches / 3 and counter < 2 * total_batches / 3:
             if counter % trimlevel == 0:
                 W_old = fastrnnbonsaiObj.W_eval.eval()
                 V_old = fastrnnbonsaiObj.V_eval.eval()
-                Z_old = fastrnnbonsaiObj.Z_eval.eval()
+                if not args.noZ:
+                    Z_old = fastrnnbonsaiObj.Z_eval.eval()
                 T_old = fastrnnbonsaiObj.T_eval.eval()
 
                 W_new = utils.hardThreshold(W_old, fastrnnbonsaiObj.sW)
                 V_new = utils.hardThreshold(V_old, fastrnnbonsaiObj.sV)
-                Z_new = utils.hardThreshold(Z_old, fastrnnbonsaiObj.sZ)
+                if not args.noZ:
+                    Z_new = utils.hardThreshold(Z_old, fastrnnbonsaiObj.sZ)
                 T_new = utils.hardThreshold(T_old, fastrnnbonsaiObj.sT)
 
                 if counter % num_iters == 0:
@@ -530,12 +754,14 @@ for i in range(num_epochs):
                 counter < 2 * total_batches / 3) and counter % trimlevel != 0) or (counter >= 2 * total_batches / 3)):
                 W_old = fastrnnbonsaiObj.W_eval.eval()
                 V_old = fastrnnbonsaiObj.V_eval.eval()
-                Z_old = fastrnnbonsaiObj.Z_eval.eval()
+                if not args.noZ:
+                    Z_old = fastrnnbonsaiObj.Z_eval.eval()
                 T_old = fastrnnbonsaiObj.T_eval.eval()
 
                 W_new1 = utils.copySupport(W_new, W_old)
                 V_new1 = utils.copySupport(V_new, V_old)
-                Z_new1 = utils.copySupport(Z_new, Z_old)
+                if not args.noZ:
+                    Z_new1 = utils.copySupport(Z_new, Z_old)
                 T_new1 = utils.copySupport(T_new, T_old)
 
                 if counter % num_iters == 0:
@@ -550,12 +776,14 @@ for i in range(num_epochs):
             counter < 2 * total_batches / 3) and counter % trimlevel != 0) or (counter >= 2 * total_batches / 3)):
             W_old = fastrnnbonsaiObj.W_eval.eval()
             V_old = fastrnnbonsaiObj.V_eval.eval()
-            Z_old = fastrnnbonsaiObj.Z_eval.eval()
+            if not args.noZ:
+                Z_old = fastrnnbonsaiObj.Z_eval.eval()
             T_old = fastrnnbonsaiObj.T_eval.eval()
 
             W_new1 = utils.copySupport(W_new, W_old)
             V_new1 = utils.copySupport(V_new, V_old)
-            Z_new1 = utils.copySupport(Z_new, Z_old)
+            if not args.noZ:
+                Z_new1 = utils.copySupport(Z_new, Z_old)
             T_new1 = utils.copySupport(T_new, T_old)
 
             if counter % num_iters == 0:
@@ -565,7 +793,7 @@ for i in range(num_epochs):
                 np.count_nonzero(W_new) + np.count_nonzero(V_new) + np.count_nonzero(Z_new) + np.count_nonzero(
                     T_new)) / 1024.0)
             fd_st = {fastrnnbonsaiObj.W_st: W_new1, fastrnnbonsaiObj.V_st: V_new1, fastrnnbonsaiObj.Z_st: Z_new1, fastrnnbonsaiObj.T_st: T_new1}
-            sess.run(fastrnnbonsaiObj.sparse_retrain_grp, feed_dict=fd_st)
+            sess.run(fastrnnbonsaiObj.sparse_retrain_grp, feed_dict=fd_st)'''
 
         counter = counter + 1
 
@@ -627,11 +855,19 @@ print(max_acc,max_try_acc)
 '''
 print('Best accuracy:', max_acc)
 # Create result string
+bonsai_model_size = fastrnnbonsaiObj.getModelSize()
+if args.noZ:
+    projectionDimension = None
+    regZ = None
+    sparZ = None
 results_list = [args.ggnl, args.gunl, args.ur, args.wr, args.w, args.sp, args.lr, args.bs, args.hs, args.ot,
-       args.ml, args.fn, max_acc]
+       args.ml, args.fn, sigma, depth, projectionDimension, regZ, regT, regW, regV, sparZ, sparW, sparV, sparT, bonsai_model_size, max_acc]
 
 # Print to output file
 out_handle = open(args.out, "a")
 # Write a line of output
 out_handle.write('\t'.join(map(str, results_list)) + '\n')
 out_handle.close()
+
+# Program running time
+print("--- %s minutes ---" % ((time.time() - start_time)/60.0))
---
+++ hpc_scripts/0_gridsearch_fastgrnn_bonsai_noZ.sh
@@ -0,0 +1,82 @@
+#!/usr/bin/env bash
+###############################################################################################################################################################
+# Hyperparameters: ref: https://github.com/Microsoft/EdgeML/blob/master/docs/README_BONSAI_OSS.md
+###############################################################################################################################################################
+# Bonsai [Options] DataFolder
+# Options:
+#
+# -F    : [Required] Number of features in the data.
+# -C    : [Required] Number of Classification Classes/Labels.
+# -nT   : [Required] Number of training examples.
+# -nE   : [Required] Number of examples in test file.
+# -f    : [Optional] Input format. Takes two values [0 and 1]. 0 is for libsvm_format(default), 1 is for tab/space separated input.
+#
+# -P   : [Optional] Projection Dimension. (Default: 10 Try: [5, 20, 30, 50])
+# -D   : [Optional] Depth of the Bonsai tree. (Default: 3 Try: [2, 4, 5])
+# -S   : [Optional] sigma = parameter for sigmoid sharpness  (Default: 1.0 Try: [3.0, 0.05, 0.005] ).
+#
+# -lW  : [Optional] lW = regularizer for predictor parameter W  (Default: 0.0001 Try: [0.01, 0.001, 0.00001]).
+# -lT  : [Optional] lTheta = regularizer for branching parameter Theta  (Default: 0.0001 Try: [0.01, 0.001, 0.00001]).
+# -lV  : [Optional] lV = regularizer for predictor parameter V  (Default: 0.0001 Try: [0.01, 0.001, 0.00001]).
+# -lZ  : [Optional] lZ = regularizer for projection parameter Z  (Default: 0.00001 Try: [0.001, 0.0001, 0.000001]).
+#
+# Use Sparsity Params to vary your model size for a given tree depth and projection dimension
+# -sW  : [Optional] lambdaW = sparsity for predictor parameter W  (Default: For Binaay 1.0 else 0.2 Try: [0.1, 0.3, 0.4, 0.5]).
+# -sT  : [Optional] lambdaTheta = sparsity for branching parameter Theta  (Default: For Binary 1.0 else 0.2 Try: [0.1, 0.3, 0.4, 0.5]).
+# -sV  : [Optional] lambdaV = sparsity for predictor parameters V  (Default: For Binary 1.0 else 0.2 Try: [0.1, 0.3, 0.4, 0.5]).
+# -sZ  : [Optional] lambdaZ = sparsity for projection parameters Z  (Default: 0.2 Try: [0.1, 0.3, 0.4, 0.5]).
+#
+# -I   : [Optional] [Default: 42 Try: [100, 30, 60]] Number of passes through the dataset.
+# -B   : [Optional] Batch Factor [Default: 1 Try: [2.5, 10, 100]] Float Factor to multiply with sqrt(ntrain) to make the batch_size = min(max(100, B*sqrt(nT)), nT).
+# DataFolder : [Required] Path to folder containing data with filenames being 'train.txt' and 'test.txt' in the folder."
+#
+# Note - Both libsvm_format and Space/Tab separated format can be either Zero or One Indexed in labels. To use Zero Index enable ZERO_BASED_IO flag in config.mk
+# and recompile Bonsai
+###############################################################################################################################################################
+
+########################################################
+# Check number of args
+########################################################
+if [ $# -ne 1 ]
+then
+	echo "Usage: $0 <pathname>"
+	exit 1
+fi
+
+# Overall parameter space (reduced) #
+#lW=(0.00001 0.0001 0.001 0.01)
+#lV=(0.00001 0.0001 0.001 0.01)
+#lT=(0.00001 0.0001 0.001 0.01)
+#lZ=(0.000001 0.00001 0.0001 0.001)
+#sZ=(0.1 0.2 0.3 0.4 0.5)
+
+
+# Parameter space (reduced)#
+D=(2 3)
+S=(1.0)
+lW=(0.00001 0.0001 0.001 0.01)
+lV=(0.00001 0.0001 0.001 0.01)
+lT=(0.00001 0.0001 0.001 0.01)
+
+# Output filename
+echo 'outname=`echo $0 | sed "s/.sh/.out/g"`'
+
+for j in ${D[@]};
+do
+    for k in ${S[@]};
+    do
+        for l in ${lW[@]};
+        do
+            for m in ${lV[@]};
+            do
+                for n in ${lT[@]};
+                do
+                    for r in `seq 1 5`;
+                    do
+                        echo python3 ../Architecture/dynamic_rnn_bonsai.py -noZ -base $1 -ct 1 -w 32 -sp 0.5 -lr 0.005 -bs 128 -hs 16 -ot 1 -ml 768 -fn $r -dep $j -sig $k -rW $l -rV $m -rT $n -out '$outname'
+                    done
+                done
+            done
+        done
+    done
+done
