dcee351b6a7285cbef86383ad6bc58ee63496241
Dhrubojyoti Roy <roy.174@buckeyemail.osu.edu>
2019-05-24 17:24:18 -0400

    Saving train, val, test data

--- Scripts/create_train_val_test_split.py
+++ Scripts/create_train_val_test_split.py
@@ -1,4 +1,5 @@
 from sklearn.model_selection import train_test_split
+from shutil import copy
 import numpy as np
 import argparse
 import os
@@ -32,11 +33,20 @@ fileloc = args.base
 #filestrs = [args.hum, args.nhum]
 filestrs = args.cldirs
 
-data = [];
-labels = [];
+data = []
+labels = []
+filenames = []
+
+# Filenames
+[[filenames.append(os.path.join(os.path.join(fileloc, filestr), filename))
+  for filename in os.listdir(os.path.join(fileloc, filestr))] for filestr in filestrs]
+
+# Data
 [[data.append(np.fromfile(open(os.path.join(os.path.join(fileloc, filestr), filename), "r"), dtype=np.uint16).tolist())
-  for filename in os.listdir(os.path.join(fileloc, filestr))] for filestr in filestrs];
+  for filename in os.listdir(os.path.join(fileloc, filestr))] for filestr in filestrs]
 data = np.array(data, dtype=object);
+
+# Labels
 [labels.extend([i] * [None for filename in os.listdir(os.path.join(fileloc, filestrs[i]))].__len__()) for i in
  range(filestrs.__len__())]
 
@@ -44,8 +54,11 @@ data = np.array(data)
 print('Total number of data points: {}'.format(data.__len__()))
 labels = np.array(labels)
 
-X_train, X_test, y_train, y_test = train_test_split(data, labels, test_size=args.spl, random_state=42)
-X_train, X_val, y_train, y_val = train_test_split(X_train, y_train,
+indices = np.arange(len(filenames))
+
+X_train, X_test, y_train, y_test, indices_train, indices_test = train_test_split(data, labels, indices,
+                                                test_size=args.spl, random_state=42)
+X_train, X_val, y_train, y_val, indices_train, indices_val = train_test_split(X_train, y_train, indices_train,
                                                   test_size=args.spl*(len(y_train)+len(y_test))/len(y_train), random_state=42)
 
 # Prefix
@@ -68,3 +81,25 @@ np.save(os.path.join(outdir, prefix, prefix + "_test_lbls.npy"), y_test)
 np.save(os.path.join(outdir, prefix, prefix + "_val.npy"), X_val)
 np.save(os.path.join(outdir, prefix, prefix + "_val_lbls.npy"), y_val)
 
+
+# Save train, test, validation cuts
+files_train = [filenames[i] for i in indices_train]
+files_val = [filenames[i] for i in indices_val]
+files_test = [filenames[i] for i in indices_test]
+
+train_dir = os.path.join(outdir, prefix, prefix + "_train")
+test_dir = os.path.join(outdir, prefix, prefix + "_test")
+val_dir = os.path.join(outdir, prefix, prefix + "_val")
+
+os.makedirs(train_dir, exist_ok=True)
+os.makedirs(test_dir, exist_ok=True)
+os.makedirs(val_dir, exist_ok=True)
+
+for tr in files_train:
+    copy(tr, train_dir)
+
+for tst in files_test:
+    copy(tst, test_dir)
+
+for val in files_val:
+    copy(val, val_dir)
