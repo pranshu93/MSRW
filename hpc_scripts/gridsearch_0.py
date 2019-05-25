5a0b82129961d795e5b20fe8a8e61d56208c937a
Dhrubojyoti Roy <roy.174@buckeyemail.osu.edu>
2019-05-25 16:05:19 -0400

    Simplifying name of gridsearch_tvt_0 to gridsearch_0 For the time being

--- hpc_scripts/gridsearch_0.py
+++ hpc_scripts/gridsearch_0.py
@@ -13,7 +13,7 @@ parser.add_argument('-bat', type=str, default='pbs', help='Batch system (pbs/slu
 parser.add_argument('-big', type=ast.literal_eval, default=False, help='Is this a big gridsearch?')
 parser.add_argument('-q15', type=ast.literal_eval, default=False, help='Is this a Q15 gridsearch?')
 
-if len(sys.argv)<2:
+if len(sys.argv) < 2:
     parser.print_help()
     sys.exit(1)
 
@@ -27,48 +27,46 @@ else:
     p13 = ['tanh', 'sigmoid']
 
 if args.big:
-    p14 = [0.25,0.75] #[0.25,0.5,0.75,1] #[1]
-    p15 = [0.25,0.75] #[0.25,0.5,0.75,1] #[1]
-    p2 =  [32,64,96,128] #[32]
-    p3 =  [0.25,0.5,0.75,1] #[0.5]
-    p4 = [0.005] #[0.005,0.01]
-    p5 = [32,64,96,128] #[128]
-    p6 = [16,48] #[16,32,48,64] #[16]
-    p7 = [0] #[1]
-    p8 = [512,768,1024] #[768]
+    p14 = [0.25, 0.75]  # [0.25,0.5,0.75,1] #[1]
+    p15 = [0.25, 0.75]  # [0.25,0.5,0.75,1] #[1]
+    p2 = [32, 64, 96, 128]  # [32]
+    p3 = [0.25, 0.5, 0.75, 1]  # [0.5]
+    p4 = [0.005]  # [0.005,0.01]
+    p5 = [32, 64, 96, 128]  # [128]
+    p6 = [16, 48]  # [16,32,48,64] #[16]
+    p7 = [0]  # [1]
+    # p8 = [512,768,1024] #[768]
 else:
     p14 = [0.25, 0.75]
     p15 = [0.25, 0.75]
-    p2 = [32]
+    p2 = [32, 64, 96, 128]
     p3 = [0.5]
     p4 = [0.005]
-    p5 = [128]
-    p6 = [16,32]
-    p7 = [0,1]
-    p8 = [768] # no longer used
+    p5 = [64, 128]
+    p6 = [16,32,48,64]
+    p7 = [0]
+    # p8 = [768] # no longer used
 
-out_folder = os.path.join('..',args.bat+'_hpc')
-out_suffix=''
+out_folder = os.path.join('..', args.bat + '_hpc')
+out_suffix = ''
 if args.big:
-    out_suffix+='_big'
+    out_suffix += '_big'
 if args.q15:
     out_suffix += '_q15'
-out_file = os.path.join('..', args.bat+'_hpc', args.type + out_suffix + '.sh')
+out_file = os.path.join('..', args.bat + '_hpc', args.type + out_suffix + '.sh')
+
 
 def generate_trainstring(v):
-    call_str = "python3 ../Architecture/dynamic_rnn.py -ggnl " + str(
+    res_str = "python3 ../Architecture/dynamic_rnn_tvt.py -ggnl " + str(
         v[0]) + " -gunl " + str(v[1]) + " -ur " + str(v[2]) + " -wr " + str(v[3]) + " -w " + str(
         v[4]) + " -sp " + str(v[5]) + " -lr " + str(v[6]) + " -bs " + str(v[7]) + " -hs " + str(
-        v[8]) + " -ot " + str(v[9]) + " -ml " + str(v[10])
-
-    res_str=[]
-    for i in range(1,6):
-        res_str.append(call_str + " -fn " + str(i) + " -type " + args.type + " -q15 " + str(args.q15) + " -base " + args.base + " -out $outname")
+        v[8]) + " -ot " + str(v[9]) + " -type " + args.type + " -q15 " + str(
+        args.q15) + " -base " + args.base + " -out $outname"
 
     return res_str
 
 pool = ThreadPool()
-hyperparams = np.asarray(list(itertools.product(p12,p13,p14,p15,p2,p3,p4,p5,p6,p7,p8))).tolist()
+hyperparams = np.asarray(list(itertools.product(p12, p13, p14, p15, p2, p3, p4, p5, p6, p7))).tolist()
 results = pool.map(generate_trainstring, hyperparams)
 
 # Flatten
@@ -77,4 +75,4 @@ results = [item for sublist in results for item in sublist]
 with open(out_file, 'w') as f:
     print('outname=`echo $0 | sed "s/.sh/.out/g"`', file=f)
 with open(out_file, 'a') as f:
-    print(*results, sep = "\n", file=f)
+    print(*results, sep="\n", file=f)
