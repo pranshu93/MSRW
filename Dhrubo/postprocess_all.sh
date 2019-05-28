62e51e83073e81b537db04c41a5bf6264f2a34bd
Dhrubojyoti Roy <roy.174@buckeyemail.osu.edu>
2019-05-27 20:09:14 -0400

    Modifying postprocessing script for TVT

--- Dhrubo/postprocess_all.sh
+++ Dhrubo/postprocess_all.sh
@@ -1,18 +1,16 @@
 #!/usr/bin/env bash
 
 list_files=(
+            bb_tar_winlen_256_winindex_all
             bb_tar_winlen_384_winindex_all
             bb_tar_winlen_512_winindex_all
             bb_tar_winlen_640_winindex_all
             bb_tar_winlen_768_winindex_all
-            bb_tar_winlen_896_winindex_all
-            bb_tar_winlen_1024_winindex_all
-            bb_tar_winlen_1152_winindex_all
             )
 
 for l in ${list_files[@]}; do
     echo -e "\n\n-------------------- Processing $l ---------------------"
     sh ../hpc_scripts/4_collate_output_splits.sh $l > $l.out
 
-    python3 ../hpc_scripts/5_compute_cv.py $l.out
+    python3 ../hpc_scripts/5_compute_best_tvt.py $l.out
 done
