ceefd0e36af1f605c8a5bfa0f7d5fbf6df9029ec
Dhrubojyoti Roy <roy.174@buckeyemail.osu.edu>
2019-06-03 16:26:43 -0400

    Adding postprocessing for 3class

---
+++ Dhrubo/postprocess_all_3class.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+source activate tfgpu
+
+list_files=(
+            bb_3class_winlen_256_winindex_all
+            bb_3class_winlen_384_winindex_all
+            bb_3class_winlen_512_winindex_all
+            bb_3class_winlen_640_winindex_all
+            bb_3class_winlen_768_winindex_all
+            )
+
+outname=rerun_bb_tar_winindex_all.sh
+echo 'outname=`echo $0 | sed "s/.sh/.out/g"`' > $outname
+
+for l in ${list_files[@]}; do
+    echo -e "\n\n-------------------- Processing $l ---------------------"
+    sh ../hpc_scripts/4_collate_output_splits.sh $l > $l.out
+
+    python3 ../hpc_scripts/5_compute_best_tvt.py $l.out $outname
+done
+
+# Change pbs data path to slurm
+sed -i 's|'/fs/project/PAS1090/radar/Bumblebee/'|'/scratch/dr2915/Bumblebee/'|g' $outname
+
+sh ../hpc_scripts/1_split_hyperparam_file.sh $outname 5
+sh ../hpc_scripts/2_create_batch_jobs.sh "rerun_3class_tar_winindex_all" "02:00:00" slurm
