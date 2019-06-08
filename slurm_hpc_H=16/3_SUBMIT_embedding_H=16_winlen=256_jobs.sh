bbdbadced382af773641689d10cae05b5c62205c
Dhrubojyoti Roy <roy.174@buckeyemail.osu.edu>
2019-06-08 19:30:23 -0400

    Fixing job walltime 12 hours, not 12 mins

--- slurm_hpc_H=16/3_SUBMIT_embedding_H=16_winlen=256_jobs.sh
+++ slurm_hpc_H=16/3_SUBMIT_embedding_H=16_winlen=256_jobs.sh
@@ -1,16 +1,16 @@
-sbatch -t 12:00 --export=filename=../slurm_hpc_H=16/embedding_H=16_winlen=256_1_spl.sh batch_job.sbatch
+sbatch -t 12:00:00 --export=filename=../slurm_hpc_H=16/embedding_H=16_winlen=256_1_spl.sh batch_job.sbatch
 sleep 1
-sbatch -t 12:00 --export=filename=../slurm_hpc_H=16/embedding_H=16_winlen=256_2_spl.sh batch_job.sbatch
+sbatch -t 12:00:00 --export=filename=../slurm_hpc_H=16/embedding_H=16_winlen=256_2_spl.sh batch_job.sbatch
 sleep 1
-sbatch -t 12:00 --export=filename=../slurm_hpc_H=16/embedding_H=16_winlen=256_3_spl.sh batch_job.sbatch
+sbatch -t 12:00:00 --export=filename=../slurm_hpc_H=16/embedding_H=16_winlen=256_3_spl.sh batch_job.sbatch
 sleep 1
-sbatch -t 12:00 --export=filename=../slurm_hpc_H=16/embedding_H=16_winlen=256_4_spl.sh batch_job.sbatch
+sbatch -t 12:00:00 --export=filename=../slurm_hpc_H=16/embedding_H=16_winlen=256_4_spl.sh batch_job.sbatch
 sleep 1
-sbatch -t 12:00 --export=filename=../slurm_hpc_H=16/embedding_H=16_winlen=256_5_spl.sh batch_job.sbatch
+sbatch -t 12:00:00 --export=filename=../slurm_hpc_H=16/embedding_H=16_winlen=256_5_spl.sh batch_job.sbatch
 sleep 1
-sbatch -t 12:00 --export=filename=../slurm_hpc_H=16/embedding_H=16_winlen=256_6_spl.sh batch_job.sbatch
+sbatch -t 12:00:00 --export=filename=../slurm_hpc_H=16/embedding_H=16_winlen=256_6_spl.sh batch_job.sbatch
 sleep 1
-sbatch -t 12:00 --export=filename=../slurm_hpc_H=16/embedding_H=16_winlen=256_7_spl.sh batch_job.sbatch
+sbatch -t 12:00:00 --export=filename=../slurm_hpc_H=16/embedding_H=16_winlen=256_7_spl.sh batch_job.sbatch
 sleep 1
-sbatch -t 12:00 --export=filename=../slurm_hpc_H=16/embedding_H=16_winlen=256_8_spl.sh batch_job.sbatch
+sbatch -t 12:00:00 --export=filename=../slurm_hpc_H=16/embedding_H=16_winlen=256_8_spl.sh batch_job.sbatch
 sleep 1
--- slurm_hpc_H=16/3_SUBMIT_embedding_H=16_winlen=768_jobs.sh
+++ slurm_hpc_H=16/3_SUBMIT_embedding_H=16_winlen=768_jobs.sh
@@ -1,16 +1,16 @@
-sbatch -t 12:00 --export=filename=../slurm_hpc_H=16/embedding_H=16_winlen=768_1_spl.sh batch_job.sbatch
+sbatch -t 12:00:00 --export=filename=../slurm_hpc_H=16/embedding_H=16_winlen=768_1_spl.sh batch_job.sbatch
 sleep 1
-sbatch -t 12:00 --export=filename=../slurm_hpc_H=16/embedding_H=16_winlen=768_2_spl.sh batch_job.sbatch
+sbatch -t 12:00:00 --export=filename=../slurm_hpc_H=16/embedding_H=16_winlen=768_2_spl.sh batch_job.sbatch
 sleep 1
-sbatch -t 12:00 --export=filename=../slurm_hpc_H=16/embedding_H=16_winlen=768_3_spl.sh batch_job.sbatch
+sbatch -t 12:00:00 --export=filename=../slurm_hpc_H=16/embedding_H=16_winlen=768_3_spl.sh batch_job.sbatch
 sleep 1
-sbatch -t 12:00 --export=filename=../slurm_hpc_H=16/embedding_H=16_winlen=768_4_spl.sh batch_job.sbatch
+sbatch -t 12:00:00 --export=filename=../slurm_hpc_H=16/embedding_H=16_winlen=768_4_spl.sh batch_job.sbatch
 sleep 1
-sbatch -t 12:00 --export=filename=../slurm_hpc_H=16/embedding_H=16_winlen=768_5_spl.sh batch_job.sbatch
+sbatch -t 12:00:00 --export=filename=../slurm_hpc_H=16/embedding_H=16_winlen=768_5_spl.sh batch_job.sbatch
 sleep 1
-sbatch -t 12:00 --export=filename=../slurm_hpc_H=16/embedding_H=16_winlen=768_6_spl.sh batch_job.sbatch
+sbatch -t 12:00:00 --export=filename=../slurm_hpc_H=16/embedding_H=16_winlen=768_6_spl.sh batch_job.sbatch
 sleep 1
-sbatch -t 12:00 --export=filename=../slurm_hpc_H=16/embedding_H=16_winlen=768_7_spl.sh batch_job.sbatch
+sbatch -t 12:00:00 --export=filename=../slurm_hpc_H=16/embedding_H=16_winlen=768_7_spl.sh batch_job.sbatch
 sleep 1
-sbatch -t 12:00 --export=filename=../slurm_hpc_H=16/embedding_H=16_winlen=768_8_spl.sh batch_job.sbatch
+sbatch -t 12:00:00 --export=filename=../slurm_hpc_H=16/embedding_H=16_winlen=768_8_spl.sh batch_job.sbatch
 sleep 1
